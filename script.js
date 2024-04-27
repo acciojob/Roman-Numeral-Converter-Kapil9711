745f835033e62d2af5f2ff1392818e416f92ec7e
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-04-27 12:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
 function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
@@ -8,10 +11,36 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
-  //your code here
+const keys = Object.keys(obj);
+let step = 0;
+let romanNumber = '';
+let tempNum = num;
+while(tempNum > 0){
+for(let key of keys){
+   const number = obj[key][1];
+   const roman = obj[key][0]
+//   console.log(romanNumber);
+   if(tempNum >= number){
+       tempNum = tempNum - number;
+       romanNumber += roman;
+       break;
+    //   if(tempNum % number === 0) {
+    //         const noTimes = tempNum / number;
+    //         for(let i = 1; i<= noTimes ; i++) romanNumber += roman;
+    //         tempNum = 0;
+    //         break;
+    //   }
+    //   tempNum = tempNum % number;
+    //   romanNumber += roman;
+    //   break;
+   }
+}
 
 }
+console.log(romanNumber);
+}
+
+convertToRoman(798);
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
